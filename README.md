76fd2affdf37f843fcd3be9493cf0ff577667eb1
Kaz Utashiro <kaz@utashiro.com>
2019-03-22 10:55:10 +0900

    minil new App::optex::textconv

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::optex::textconv - It's new $module
+
+# SYNOPSIS
+
+    use App::optex::textconv;
+
+# DESCRIPTION
+
+App::optex::textconv is ...
+
+# LICENSE
+
+Copyright (C) Kaz Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kaz Utashiro <kaz@utashiro.com>
---
+++ lib/App/optex/textconv.pm
@@ -0,0 +1,39 @@
+package App::optex::textconv;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::optex::textconv - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::optex::textconv;
+
+=head1 DESCRIPTION
+
+App::optex::textconv is ...
+
+=head1 LICENSE
+
+Copyright (C) Kaz Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kaz Utashiro E<lt>kaz@utashiro.comE<gt>
+
+=cut
+
