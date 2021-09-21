c23f871208551f76b6fc1e6842f175d822200373
Kazumasa Utashiro <kaz@utashiro.com>
2021-09-21 17:29:43 +0900

    Support docm,pptm,xlsm extensions.

--- lib/App/optex/textconv/tika.pm
+++ lib/App/optex/textconv/tika.pm
@@ -9,9 +9,9 @@ use Carp;
 use App::optex::textconv::Converter 'import';
 
 our @CONVERTER = (
-    [ qr/\.docx$/ => \&to_text ],
-    [ qr/\.pptx$/ => \&to_text ],
-    [ qr/\.xlsx$/ => \&to_text ],
+    [ qr/\.doc[xm]$/ => \&to_text ],
+    [ qr/\.ppt[xm]$/ => \&to_text ],
+    [ qr/\.xls[xm]$/ => \&to_text ],
     );
 
 sub to_text {
