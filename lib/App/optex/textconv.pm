d0e9ec22105863b6a6bd07cfd9ac541d895b713f
Kazumasa Utashiro <kaz@utashiro.com>
2022-06-21 19:48:57 +0900

    Checking in changes prior to tagging of version 1.03.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 23c734d..4af92b8 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension App-optex-textconv
     
     {{$NEXT}}
     
    +1.03 2022-06-21T10:48:49Z
    +
    +    - Simplified @ARGV handling.
    +
     1.02 2022-01-13T02:17:44Z
     
         - Simplified algorithm.

--- lib/App/optex/textconv.pm
+++ lib/App/optex/textconv.pm
@@ -1,6 +1,6 @@
 package App::optex::textconv;
 
-our $VERSION = '1.02';
+our $VERSION = '1.03';
 
 use v5.14;
 use warnings;
@@ -16,7 +16,7 @@ textconv - optex module to replace document file by its text contents
 
 =head1 VERSION
 
-Version 1.02
+Version 1.03
 
 =head1 SYNOPSIS
 
